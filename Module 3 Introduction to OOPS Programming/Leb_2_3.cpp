351b619b3e92f6baddb707b8ecae21a00e4b5dd9
Ayushdhaduk <Ayushdhaduk07@gmail.com>
2025-08-25 12:56:36 +0530

    Update Leb_2_3.cpp

--- Module 3 Introduction to OOPS Programming/Leb_2_3.cpp
+++ Module 3 Introduction to OOPS Programming/Leb_2_3.cpp
@@ -1,3 +1,6 @@
+//Write a C++ program that demonstrates arithmetic, relational, logical, and bitwise 
+// operators. Perform operations using each type of operator and display the results. 
+// o Objective: Reinforce understanding of different types of operators in C++. 
 #include <iostream>
 using namespace std;
 
@@ -39,4 +42,5 @@ int main() {
 
     return 0;
 }
+
 
