36bc232775e8d78ea9445fa14f6849e21a885f46
Ayushdhaduk <Ayushdhaduk07@gmail.com>
2025-08-25 12:47:01 +0530

    Update Leb_4_3.cpp

--- Module 3 Introduction to OOPS Programming/Leb_4_3.cpp
+++ Module 3 Introduction to OOPS Programming/Leb_4_3.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int globalVar = 100;
 
-void showScope() {
-    int localVar = 50;  
-    cout << "Inside showScope():" << endl;
-    cout << "Local Variable = " << localVar << endl;
-    cout << "Global Variable = " << globalVar << endl;
-}
 
-int main() {
-    int localVar = 10;  
-    cout << "Inside main():" << endl;
-    cout << "Local Variable = " << localVar << endl;
-    cout << "Global Variable = " << globalVar << endl;
-    cout<< endl;
+void showVariables();
 
-    showScope();
-
-    globalVar = 200;
-    cout << "\nAfter modifying globalVar in main():" << endl;
-    cout << "Global Variable = " << globalVar << endl;
+int main()
+ {
+    cout << "In main - Global variable: " << globalVar << endl;
+    showVariables();
 
     return 0;
 }
-
+void showVariables() {
+    int localVar = 50;  // Local variable
+    cout << "Inside function - Local variable: " << localVar << endl;
+    
+}
